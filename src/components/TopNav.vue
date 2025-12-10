6134984b6c20c4233a654bcb0d2b32613374fed1
Rohan Surti <surt0008@algonquilive.com>
2025-12-10 13:41:12 -0500

    FinalProject Commit

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/Logo.png" alt="Best Buy Page Logo">
       </a>
     </div>
     <button class="hamburger" @click="toggleNav">
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #3c673cd7;
+  background-color: #005ddfd7;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
